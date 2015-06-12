59e4534c7d9c72a7d27e539e6a722ac48871de33
SyntaxRules <devyn.stott@gmail.com>
2015-06-11 21:34:24 -0700

    fix(): angular variable undefined when window.angular is used
    There was a bug in the code where we accidently created an angular
    variable inside of the function scope, this was preventing window.angular
    or window.showdown from working. (But it was working fine with node
    and require). I wrapped the file in a IIFE that injects angular and showdown.
    This solves the problem.
    
    No breaking changes.

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,8 +10,7 @@ module.exports = function (grunt) {
     concat: {
       options: {
         sourceMap: true,
-        banner: ';/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n(function(){\n',
-        footer: '}).call(this);'
+        banner: ';/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
       },
       dist: {
         src:  [
