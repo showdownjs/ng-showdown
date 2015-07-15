522fe4745d3ddbbbf9e528072d5a5550c6bc7809
SyntaxRules <devyn.stott@gmail.com>
2015-07-14 21:40:37 -0700

    test(sdMarkdownToHtml): directive testing
    testing that it exists, has isolated scope and parses some markdown
    
    No breaking changes.

--- test/browser/ng-showdown.spec.js
+++ test/browser/ng-showdown.spec.js
@@ -1,40 +1,92 @@
 describe('Showdown', function () {
   var expect = window.expect,
-    someProvider,
-    somefilter;
-
+    showdownProvider;
   beforeEach(function () {
 
-    module('ng-showdown');
-
-    module(function ($showdownProvider) {
-      someProvider = $showdownProvider;
+    module('ng-showdown', function ($showdownProvider) {
+      showdownProvider = $showdownProvider;
     });
 
-    inject(function ($filter) {
-      somefilter = $filter('sdStripHtml');
-    });
   });
 
   afterEach(function () {
 
   });
 
-  describe('Showdown Provider', function () {
-    it('should fail', function () {
-      console.log(someProvider);
-      expect(someProvider.$get().test).to.be.equal('test');
+  describe('Showdown directive', function () {
+    var scope,
+      compile,
+      element,
+      validTemplate = '<div sd-model-to-html="someData"></div>',
+      markdown = '# Title',
+      htmlExpeted = '<h1 id="title">Title</h1>';
+
+    beforeEach(function () {
+      // Inject in angular constructs otherwise,
+      //  you would need to inject these into each test
+      inject(function ($rootScope, $compile) {
+        scope = $rootScope.$new();
+        compile = $compile;
+      });
     });
+
+    // JavaScript
+    it('should have expected isolated scope', function () {
+      element = createDirective(markdown);
+      var isolated = element.isolateScope();
+      expect(isolated.model).to.be.equal(markdown);
+    });
+
+    it('should parse markdown', function () {
+      element = createDirective(markdown);
+      expect(element.html()).to.be.equal(htmlExpeted);
+    });
+
+    function createDirective(data, template) {
+      var elm;
+
+      // Setup scope state
+      scope.someData = data;
+
+      // Create directive
+      elm = compile(template || validTemplate)(scope);
+
+      // Trigger watchers
+      scope.$apply();
+
+      // Return
+      return elm;
+    }
+  });
+
+  describe('Showdown Provider', function () {
+
+    it('should not have a test variable', function () {
+      expect(showdownProvider.$get().test).to.be.an('undefined');
+    });
+
   });
 
   describe('Showdown filter', function () {
+    var sdStripHtmlFilter;
+
+    beforeEach(function () {
+
+      inject(function ($filter) {
+        sdStripHtmlFilter = $filter('sdStripHtml');
+      });
+
+    });
+
     it('should exist', function () {
-      expect(!!somefilter).to.be.equal(true);
+      expect(!!sdStripHtmlFilter).to.be.equal(true);
     });
 
     //Not testing full functionality here, the showdown provider gets those tests
     it('should strip html', function () {
-      expect(somefilter('<p>Foo bar</p>')).to.be.equal('Foo bar');
+      expect(sdStripHtmlFilter('<p>Foo bar</p>')).to.be.equal('Foo bar');
     });
+
   });
+
 });
